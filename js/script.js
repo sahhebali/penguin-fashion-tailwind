a0cffb98979aea94f46c88720ae3ba5f8fd76349
sahhebali <saheb.co21@gmail.com>
2021-09-16 23:15:50 +0530

    penguin-faghion-telwind

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// const penguins = document.getElementById('penguins');
+// console.log(penguins);
+
+document.getElementById('button').addEventListener('click', function () {
+    const penguinsHeading = document.getElementById('penguins-heading');
+    penguinsHeading.innerText = 'I am penguin';
+})
